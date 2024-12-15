ab02d768b392c441d40860e597ec215030cee29c
Andriy Pylo <py10.dev@gmail.com>
2024-12-15 23:11:26 +0100

    Fix bug that after changing user the app still shows previous user.
    Signed-off-by: Andriy Pylo <py10.dev@gmail.com>

--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -1,12 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:instagram_clone_flutter/presentation/screens/screens.dart';
 
-String currentUserUid = FirebaseAuth.instance.currentUser!.uid;
-
 final homeScreenItems = [
   HomeScreen(),
   SearchScreen(),
   AddPostScreen(),
   ReelsScreen(),
-  ProfileScreen(uid: currentUserUid),
+  ProfileScreen(uid: FirebaseAuth.instance.currentUser!.uid),
 ];
